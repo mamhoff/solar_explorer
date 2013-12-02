bdb56abfc12529a3dbf97fe722c489024569dc1d
Martin Meyerhoff <mamhoff@googlemail.com>
2013-12-02 12:46:43 +0100

    just checkin'

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ SolarExplorer::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/imprint"
   get "static_pages/boat"
+  
+#  resources :admins
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
