9867aee81ef6ecddec335ffd036cf6c49dad2498
Martin Meyerhoff <mamhoff@googlemail.com>
2013-12-03 18:22:59 +0100

    added a Role model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+roles = Role.create([{name: "Admin"}, {name: "Guide"}, {name: "Captain"}])
+
--- spec/factories.rb
+++ spec/factories.rb
@@ -6,3 +6,8 @@ FactoryGirl.define do
     password_confirmation "foobar"
   end
 end
+FactoryGirl.define do
+  factory :role do
+  	name	"Admin"
+  end
+end
