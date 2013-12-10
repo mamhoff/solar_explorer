2627c6093d7b082d230fae9822d4fe64397ba635
Martin Meyerhoff <mamhoff@googlemail.com>
2013-12-10 13:07:50 +0100

    Tour model primitive test added

--- spec/models/tour_spec.rb
+++ spec/models/tour_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe Tour do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:tour) { FactoryGirl.create(:tour) }
+
+  subject { tour }
+
+  it { should respond_to(:name) } 
 end
