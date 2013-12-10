a8250ce758f75d22dc001f4c53e9d5b5cded04c0
Martin Meyerhoff <mamhoff@googlemail.com>
2013-12-10 13:12:22 +0100

    tours have to have a unique name (done)

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,2 +1,4 @@
 class Tour < ActiveRecord::Base
+	validates :name, uniqueness: { case_sensitive: false }, presence: true
+
 end
--- spec/models/tour_spec.rb
+++ spec/models/tour_spec.rb
@@ -5,5 +5,14 @@ describe Tour do
 
   subject { tour }
 
-  it { should respond_to(:name) } 
+  it { should respond_to(:name) }
+
+  it { should be_valid }
+
+describe "when name is not present" do
+	before { tour.name = " " }
+	it {should_not be_valid}
+end
+
+ 
 end
